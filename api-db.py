a0ae8fdd662d69c183c5e302b40acfdc76f3dd43
LolaEnBeta <lolitaenbeta@gmail.com>
2019-06-21 15:10:03 +0200

    Create route to save new user in DB

--- api-db.py
+++ api-db.py
@@ -1,15 +1,38 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 import sqlite3
 
 app = Flask(__name__)
-
-conn = sqlite3.connect("***/***.db")
-
-query = conn.cursor()
 
 @app.route("/")
 def index():
     return "Hello world!!"
 
+@app.route("/users", methods=["POST"])
+def create_user():
+    conn = sqlite3.connect("***/***.db")
+
+    query = conn.cursor()
+
+    name = request.json.get("name")
+    age = request.json.get("age")
+
+    try:
+        age = int(age)
+    except:
+        print("This is not a number")
+        exit()
+
+    arguments = (name, age)
+
+    sql = """
+    INSERT INTO users (name, age)
+    VALUES (?, ?)
+    """
+
+    if (query.execute(sql, arguments)):
+        return "User created successfully"
+    else:
+        return "An error has ocurred"
+
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
